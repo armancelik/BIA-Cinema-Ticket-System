b133c1e77de1dc05b198154fcbd287b9abf94c4d
Arman Çelik <idrisarman000@gmail.com>
2022-07-04 23:02:28 +0300

    unneccessary files remote

--- ddd/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ddd.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
